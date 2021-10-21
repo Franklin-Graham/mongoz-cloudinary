9642243cae1996d362ecec531351afb5933da062
Franklin Graham <franklingraham39@gmail.com>
2021-10-21 22:08:22 +0530

    fifth commit

--- project_1/settings.py
+++ project_1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-clr07)ytz-1h2lr)w=&tfzcc=c1r!llbk)%*znt9%8ch42_ln@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','franci98.herokuapp.com']
 
