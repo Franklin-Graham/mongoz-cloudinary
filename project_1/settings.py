c3db6d0a035cd5e23cffab480c97197090e49537
Franklin Graham <franklingraham39@gmail.com>
2021-10-21 22:38:37 +0530

    seventh commit

--- project_1/settings.py
+++ project_1/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'project_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project_5',
-        'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PORT': '3306',
-        'PASSWORD': '1234',
-
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'project_5',
+#         'HOST': '127.0.0.1',
+#         'USER': 'root',
+#         'PORT': '3306',
+#         'PASSWORD': '1234',
+#
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
