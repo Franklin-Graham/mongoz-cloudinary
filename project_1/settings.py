45b84d0cbcda09c0b479172a590db21ecb82e6eb
Franklin Graham <franklingraham39@gmail.com>
2021-10-22 11:06:20 +0530

    seventh commit

--- project_1/settings.py
+++ project_1/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'project_1.wsgi.application'
 # }
 
 import mongoengine
-mongoengine.connect(db='mongodb',username='franklingraham329@gmail.com', password="mongodb+srv://franklin:Franklin@329@franklin.j8xgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoengine.connect(db='web-project',username='franklingraham329@gmail.com', password="mongodb+srv://franklin:Franklin@329@franklin.j8xgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
